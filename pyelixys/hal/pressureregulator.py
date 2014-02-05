ae3c9aca11fb27b3d833a12138c2fd3b43964bf6
Luis Fuentes <luis90fuentes@gmail.com>
2014-02-04 19:40:53 -0800

    Changes to the callbacks for the DAC. Adding two variables to the pressure regulator variable in hwconf.ini. Need to implement correct get_pressure function. Working version of the evap run().

--- pyelixys/hal/pressureregulator.py
+++ pyelixys/hal/pressureregulator.py
@@ -10,12 +10,15 @@ from the ADCs can be used to determine if right
 pressure it obtained for continuing.
 """
 import time
+from datetime import datetime
+from datetime import timedelta
+
+from threading import Thread
 
 from pyelixys.logs import hallog as log
 from pyelixys.hal.systemobject import SystemObject
 from pyelixys.elixysexceptions import ElixysPneumaticError, \
                                       ElixysGasTransferError
-
 
 class PressureRegulator(SystemObject):
     """ The PressureRegulator can be used to
@@ -30,20 +33,49 @@ class PressureRegulator(SystemObject):
     def __init__(self, devid, synthesizer):
         """ (Construct) """
         self.id_ = devid
+        
         super(PressureRegulator, self).__init__(synthesizer)
 
     def _get_conf(self):
         """ Return the config this pressure regulator """
         self.press_conf = self.sysconf['PressureRegulators']
+        self.allowable_pressure_diff = (
+                self.press_conf
+                ['PressureRegulator' + str(self.id_)]
+                ['allowable_pressure'])
+        self.allowable_delay = timedelta(
+                0,
+                self.press_conf
+                ['PressureRegulator' + str(self.id_)]
+                ['allowable_delay'])
+
         return self.press_conf['PressureRegulator%d' % self.id_]
     
     def set_setpoint(self, value):
         """ Set the pressure regulator,
         setpoint is generally in psi, but really
         depends on the hw configuration """
+        
+        
         value = value * self.conf['PSICONV']
         log.debug("Set pressure regulator %d to %f", self.id_, value)
         self.synth.cbox.set_dac(self.id_, value) 
+        return
+
+        log.debug("Checking for pressure to equal setpoint")
+        # Loop until the pressure reaches the setpoint
+        begintime = datetime.now()
+        is_pressure_reached = False
+        while (self.allowable_delay > datetime.now() - begintime
+                and not is_pressure_reached):
+            if (self.allowable_pressure_diff <= self.get_pressure()
+                    and self.get_pressure() <= self.get_pressure()):
+                is_pressure_reached = True
+        if is_pressure_reached:
+            log.debug("Sucessfully set pressure: %s", self.get_setpoint())
+        else:
+            log.debug("Unable to set pressure to %s", self.get_setpoint())
+
 
     def get_setpoint(self):
         """ Get the pressure regulator
@@ -56,6 +88,31 @@ class PressureRegulator(SystemObject):
         return value
 
     setpoint = property(get_setpoint, set_setpoint)
+    
+    def set_pressure(self, value):
+        '''
+        Set the pressure regulator pressure by
+        setting the setpoint and confirming the
+        actual pressure reaches the setpoint.
+        '''
+        # Slowly increment the pressure to
+        # simulate the increase of pressure
+        # until we reach it or timeout
+        increment_value = value/10
+        begintime = datetime.now()
+        is_pressure_reached = False
+        while (self.allowable_delay > datetime.now() - begintime
+                and not is_pressure_reached):
+            self.set_setpoint(increment_value)
+            if (self.allowable_pressure_diff <= self.get_pressure()
+                    and self.get_pressure() <= self.get_pressure()):
+                is_pressure_reached = True
+        
+        
+        if is_pressure_reached:
+            log.debug("Sucessfully set pressure: %s", self.get_setpoint())
+        else:
+            log.debug("Unable to set pressure to %s", self.get_setpoint())
 
     def get_pressure(self):
         """ Return the actual pressure in PSI,
@@ -67,7 +124,7 @@ class PressureRegulator(SystemObject):
                 self.id_, value)
         return value
 
-    pressure = property(get_pressure)
+    pressure = property(get_pressure, set_pressure)
 
     #: Pressure regualtor config
     conf = property(_get_conf)
--- pyelixys/sequence/components/component.py
+++ pyelixys/sequence/components/component.py
@@ -14,10 +14,8 @@ class Component(object):
         property and set log messages with
         the same string.
     """
-    system = None
-    log = None
-    #system = system
-    #log = log
+    system = system
+    log = log
 
     def __init__(self, dbcomp):
         self.dbcomp = dbcomp
@@ -29,14 +27,19 @@ class Component(object):
         pass
 
     def get_component_status(self):
-        '''
-        '''
+        '''Returns the status of the component'''
         return self.status
 
-    def set_component_status(self, new_component_status):
+    def set_component_status(
+            self,
+            new_component_status):
         '''
+        Takes in a new component status
+        and an optional level for the
+        logger. When the status is updated/set,
+        the logger should log the status as well.
         '''
-
+        log.debug(str(new_component_status))
         self.status = new_component_status
 
     component_status = property(
